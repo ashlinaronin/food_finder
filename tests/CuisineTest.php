92ae149b0d1fc0ac4b7d8c46c191c0aac7bb7649
Ashlin Aronin & Ben Casalino <student@epicodus.com>
2015-08-19 15:43:32 -0700

    Write spec for getRestaurants in Cuisine and make it fail

--- tests/CuisineTest.php
+++ tests/CuisineTest.php
@@ -109,14 +109,47 @@
             $test_cuisine2 = new Cuisine ($test_name2, $test_spicy2, $test_price2) ;
             $test_cuisine2->save();
 
-
-
             //Act
             $result = Cuisine::getAll();
 
-
             //Assert
             $this->assertEquals([$test_cuisine, $test_cuisine2 ], $result);
+        }
+
+
+        function test_getRestaurants()
+        {
+            //Arrange
+            $cuisine_name = "Lebanese";
+            $cuisine_spicy = false;
+            $cuisine_price = 5;
+            $test_cuisine = new Cuisine($cuisine_name, $cuisine_spicy, $cuisine_price);
+            $test_cuisine->save();
+            $test_cuisine_id = $test_cuisine->getId();
+
+            $test_restaurant1 = new Restaurant(
+                "Ben's Bulkogi",
+                50,
+                "eastside",
+                true,
+                $test_cuisine_id
+            );
+            $test_restaurant1->save();
+
+            $test_restaurant2 = new Restaurant(
+                "Mario's Pizza Hole",
+                35,
+                "Hole",
+                false,
+                $test_cuisine_id
+            );
+            $test_restaurant2->save();
+
+            //Act
+            $result = $test_cuisine->getRestaurants();
+
+            //Assert
+            $this->assertEquals([$test_restaurant1, $test_restaurant2], $result);
         }
 
 
