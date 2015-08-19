31243737bf30fac7baec6f82c6d4a918dec304da
Ashlin Aronin & Ben Casalino <student@epicodus.com>
2015-08-19 15:15:48 -0700

    fail for test get all

--- tests/CuisineTest.php
+++ tests/CuisineTest.php
@@ -53,6 +53,75 @@
 
         }
 
+        function test_getPrice()
+        {
+            //Arrange
+            $test_name = "Indian";
+            $test_spicy = true;
+            $test_price = 1;
+            $test_cuisine = new Cuisine( $test_name, $test_spicy, $test_price);
+
+            //Act
+            $result = $test_cuisine->getPrice();
+
+            //Assert
+            $this->assertEquals($test_price, $result);
+
+
+        }
+
+        function test_save()
+
+        {
+            //Arrange
+            $test_name = " Itilanooooo ";
+            $test_spicy = true;
+            $test_price = 4;
+            $test_cuisine = new Cuisine ($test_name, $test_spicy, $test_price) ;
+            $test_cuisine->save();
+
+
+
+            //Act
+            $result = Cuisine::getAll();
+
+
+            //Assert
+            $this->assertEquals($test_cuisine, $result[0]);
+
+
+
+        }
+
+        function test_getAll()
+        {
+            //c1
+            //Arrange
+            $test_name = " Itilanooooo ";
+            $test_spicy = true;
+            $test_price = 4;
+            $test_cuisine = new Cuisine ($test_name, $test_spicy, $test_price) ;
+            $test_cuisine->save();
+
+            $test_name2 = " Mexican ";
+            $test_spicy2 = false;
+            $test_price2 = 3;
+            $test_cuisine2 = new Cuisine ($test_name2, $test_spicy2, $test_price2) ;
+            $test_cuisine2->save();
+
+
+
+            //Act
+            $result = Cuisine::getAll();
+
+
+            //Assert
+            $this->assertEquals([$test_cuisine, $test_cuisine2 ], $result);
+        }
+
+
+
+
 
     }
  ?>
